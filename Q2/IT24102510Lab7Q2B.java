960d07634c338c1d6a6281dfe2c77ed8f9be0de3
IT24102510 <it24102510@my.sliit.lk>
2024-09-21 23:50:10 +0530

    Add files via upload

---
+++ Q2/IT24102510Lab7Q2B.java
@@ -0,0 +1,11 @@
+public class IT24102510Lab7Q2B{
+public static void main(String[] args){
+  for(int i = 1;i<=5;i++){
+     System.out.print(i+" - ");
+     for(int t = 1;t<=i;t++){
+       System.out.print("* ");
+    }
+       System.out.println();
+   }
+  }
+}
