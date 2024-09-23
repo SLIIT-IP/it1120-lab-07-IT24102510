7d17d603f61af82e17b8fd18ec1b5ac25c1157e5
IT24102510 <it24102510@my.sliit.lk>
2024-09-23 21:19:23 +0530

    Update IT24102510Lab7Q1B.java

--- Q1/IT24102510Lab7Q1B.java
+++ Q1/IT24102510Lab7Q1B.java
@@ -1,40 +1,50 @@
 import java.util.Scanner;
-public class IT24102510Lab7Q1B{
-public static void main(String[] args){
-  Scanner scanner = new Scanner(System.in);
-
-  double totalmarks = 0;
-  int count = 0;
-  String Grade ="n";
-  int Scount = 1;
- 
-  while(Scount<5){
-  System.out.println("Student "+Scount);
-  System.out.print("Enter marks : ");
-  String input =scanner.nextLine();
-
-  Scanner inputscanner = new Scanner(input);
-  
-  while(inputscanner.hasNext() && count < 4){
-  if(inputscanner.hasNextDouble()){
-    double mark = inputscanner.nextDouble();
-    totalmarks += mark;
-    count++; 
-   }
-    
-  }
-    double Average =0;
-    Average = totalmarks/4;
-    if(Average > 75 && Average<= 100){
-     Grade ="Distinction";
-    }else if(Average> 49 && Average< 75){
-     Grade ="Credit";
-    }else
-     Grade ="Fail";
-     System.out.println("Avarage is : "+Average);
-     System.out.println("Overall Grade is : "+Grade);
-     Scount++;
-     System.out.println();
-  }
- }
-}
+public class IT24102510Lab7Q1B {
+    public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	
+	int mark1, mark2, mark3, mark4;
+	double avg;
+	String Grade;
+	int Student_num = 1;
+	
+	
+	
+	while (Student_num<=3) {
+	
+	System.out.print("Student" +Student_num);
+	System.out.println();
+	System.out.print("Enter marks:");
+	mark1 = input.nextInt();
+	mark2 = input.nextInt();
+	mark3 = input.nextInt();
+	mark4 = input.nextInt();
+	
+	
+	avg = (mark1 + mark2 + mark3 + mark4) / 4.0;
+	System.out.println("Average is: " +avg);
+	
+	if (avg>=75 && avg<=100)
+	{
+	      Grade = "Distinction";
+	}
+		else if (avg >= 50 && avg <= 74)
+		{
+		Grade = "Credit";
+		}
+    else 
+	{
+          Grade = "Fail";
+	}
+	
+	System.out.println("Overall Grade is:" +Grade);
+	System.out.println();
+	
+	Student_num += 1;
+	}
+	}
+	
+	
+	}
+	
